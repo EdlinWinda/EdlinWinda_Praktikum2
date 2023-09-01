9c1ffc719f38c480b14ab22d6f3dad44c52b2db9
EdlinWinda <143817177+EdlinWinda@users.noreply.github.com>
2023-09-01 23:20:08 +0700

    vertikal dan horizontal.dart

--- vertikal dan horizontal.dart
+++ vertikal dan horizontal.dart
@@ -5,73 +5,138 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key});
+  const MyApp({super.key});
 
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: const MyHomePage(title: 'DICKYA HIDAYAH PUTRA'),
+      debugShowCheckedModeBanner: false,
+      home: const MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatelessWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
+  const MyHomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Dickya Hidayah (Horizontal & Vertical Container)'),
+      title: Text('EDLIN WINDA')
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                ElevatedButton(
-                  child: Text('Button 1'),
-                  onPressed: () {},
-                ),
-                SizedBox(width: 10),
-                ElevatedButton(
-                  child: Text('Button 2'),
-                  onPressed: () {},
-                ),
-                SizedBox(width: 10),
-                ElevatedButton(
-                  child: Text('Button 3'),
-                  onPressed: () {},
-                ),
-              ],
-            ),
-            SizedBox(height: 10),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                ElevatedButton(
-                  child: Text('Button 4'),
-                  onPressed: () {},
-                ),
-                SizedBox(width: 10),
-                ElevatedButton(
-                  child: Text('Button 5'),
-                  onPressed: () {},
-                ),
-                SizedBox(width: 10),
-                ElevatedButton(
-                  child: Text('Button 6'),
-                  onPressed: () {},
-                ),
-              ],
-            ),
-          ],
-        ),
+      child: Row (
+      mainAxisAlignment: MainAxisAlignment.center,
+      children:<Widget>[
+      Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [ElevatedButton(
+      style: ElevatedButton.styleFrom(
+      primary: Colors.redAccent,
       ),
+      child:Text ('Button 1',
+      style: TextStyle(
+      color: Colors.black),
+      ),
+      onPressed:() {},
+      ),
+      Container(width: 10,height: 10,),
+      ElevatedButton(
+      style: ElevatedButton.styleFrom(
+      primary: Colors.red,
+      ),
+      child:Text ('Button 2',
+      style: TextStyle(
+      color: Colors.black),),
+      onPressed:() {},
+      ),],
+      ),
+      Container(width: 10,height: 10,),
+      Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [ElevatedButton(
+      style: ElevatedButton.styleFrom(
+      primary: Colors.blueAccent,
+      ),
+      child:Text ('Button 3',
+      style: TextStyle(
+      color: Colors.black),
+      ),
+      onPressed:() {},
+      ),
+      Container(width: 10,height: 10,),
+      ElevatedButton(
+      style: ElevatedButton.styleFrom(
+      primary: Colors.lightBlue,
+      ),
+      child:Text ('Button 4',
+      style: TextStyle(
+      color: Colors.black),),
+      onPressed:() {},
+      ),],
+      ),
+      Container(width: 10,height: 10,),
+      Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [ElevatedButton(
+      style: ElevatedButton.styleFrom(
+      primary: Colors.greenAccent,
+      ),
+      child:Text ('Button 5',
+      style: TextStyle(
+      color: Colors.black),
+      ),
+      onPressed:() {},
+      ),
+      Container(width: 10,height: 10,),
+      ElevatedButton(
+      style: ElevatedButton.styleFrom(
+      primary: Colors.lightGreen,
+      ),
+      child:Text ('Button 6',
+      style: TextStyle(
+      color: Colors.black),),
+      onPressed:() {},
+      ),
+      ],
+      ),
+      Container(width: 10,height: 10,),
+      ],
+      ),
+      ),
+      );
+  }
+}
+
+class MyHomePage2 extends StatelessWidget {
+  const MyHomePage2({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("EDLIN WINDA")),
+      body: Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+      Container(child: Text("HELLO BANG"),
+      color: Colors.red,
+      padding: EdgeInsets.all(20),
+      ),
+      Container(child: Text("HELLO BANG"),
+      color: Colors.yellow,
+      padding: EdgeInsets.all(20),
+      ),
+      Container(child: Text("HELLO BANG"),
+      color: Colors.green,
+      padding: EdgeInsets.all(20),
+      ),
+      Container(child: Text("HELLO BANG"),
+      color: Colors.lightBlue,
+      padding: EdgeInsets.all(20),
+      ),
+      ],)
     );
   }
 }
